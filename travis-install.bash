3520c741a17a12c91219d2198f2f223165a3b065
Neil Halelamien <neil@h7en.com>
2017-05-22 23:59:19 -0700

    newer chromedriver required to work with newer chrome

--- travis-install.bash
+++ travis-install.bash
@@ -33,6 +33,6 @@ fi
 if [ "${DRIVER}" = "tests/test_webdriver_chrome.py" ]; then
     sleep 1
 
-    FILE=`mktemp`; wget "http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip" -qO $FILE && unzip $FILE chromedriver -d ~; rm $FILE; chmod 777 ~/chromedriver;
+    FILE=`mktemp`; wget "http://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip" -qO $FILE && unzip $FILE chromedriver -d ~; rm $FILE; chmod 777 ~/chromedriver;
     export PATH=$HOME:$PATH
 fi
