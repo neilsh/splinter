7f7881d79cafc159a86af3ede4d5aca3949c477b
Neil Halelamien <neil@h7en.com>
2017-05-24 23:31:52 -0700

    another try at unzipping chromedriver and making it executable

--- travis-install.bash
+++ travis-install.bash
@@ -10,30 +10,23 @@ if [ "${DRIVER}" = "tests/test_djangoclient.py" ]; then
   pip install -q Django==${DJANGO_VERSION}
 fi
 
-#if [ "${DRIVER}" = "tests/test_webdriver_remote.py" ]; then
-  sleep 1
 
-	wget http://goo.gl/PJUZfa -O selenium-server.jar
-	java -jar selenium-server.jar > /dev/null 2>&1 &
-	sleep 1
-#fi
+wget http://goo.gl/PJUZfa -O selenium-server.jar
+java -jar selenium-server.jar > /dev/null 2>&1 &
 
-#if [ "${DRIVER}" = "tests/test_webdriver_firefox.py" ]; then
+wget https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux64.tar.gz
+mkdir geckodriver
+tar -xzf geckodriver-v0.14.0-linux64.tar.gz -C geckodriver
+mkdir $HOME/bin
+mv ./geckodriver/geckodriver $HOME/bin/geckodriver
+chmod +x $HOME/bin/geckodriver
 
-    wget https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux64.tar.gz
-    mkdir geckodriver
-    tar -xzf geckodriver-v0.14.0-linux64.tar.gz -C geckodriver
-    mkdir $HOME/bin
-    mv ./geckodriver/geckodriver $HOME/bin/geckodriver
-    chmod +x $HOME/bin/geckodriver
+# zip file contains a single file: 'chromedriver' (executable)
+#FILE=`mktemp`; wget "http://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip" -qO $FILE && unzip $FILE chromedriver -d ~; mv $HOME/chromedriver/chromedriver $HOME/bin/chromedriver; rm $FILE; chmod +x $HOME/bin/chromedriver;
+wget "http://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip" -qO ctemp.zip && unzip ctemp.zip chromedriver -d ~
+mv $HOME/chromedriver $HOME/bin/chromedriver
+chmod +x $HOME/bin/chromedriver;
 
-#fi
-
-#if [ "${DRIVER}" = "tests/test_webdriver_chrome.py" ]; then
-    sleep 1
-
-    FILE=`mktemp`; wget "http://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip" -qO $FILE && unzip $FILE chromedriver -d ~; mv chromedriver $HOME/bin/chromedriver; rm $FILE; chmod +x $HOME/bin/chromedriver;
-#fi
 export PATH=$HOME:$HOME/bin:$PATH
 
 ls -l $HOME/bin
